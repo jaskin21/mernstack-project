da409f26ccc62a795a2134bbf69318d3753179b1
Jay Are Galinada <jayaregalinada@gmail.com>
2022-06-19 00:55:55 +0800

    fix: All incorrect props and attributes

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const AboutPage = () => {
   return (
-    <section className='text-gray-400  body-font'>
-      <div className='container mx-auto flex px-5 py-9 items-center justify-center flex-col'>
-        <h2 className='mb-10 text-7xl font-semibold tracking-tighter text-center text-gray-200 lg:text-7xl md:text-6xl'>
-          "Quick, and Simple"
+    <section className="text-gray-400  body-font">
+      <div className="container mx-auto flex px-5 py-9 items-center justify-center flex-col">
+        <h2 className="mb-10 text-7xl font-semibold tracking-tighter text-center text-gray-200 lg:text-7xl md:text-6xl">
+          &quot;Quick, and Simple&quot;
         </h2>
 
-        <div className='text-center lg:w-2/3 w-full'>
-          <p className='leading-relaxed mb-8'>
+        <div className="text-center lg:w-2/3 w-full">
+          <p className="leading-relaxed mb-8">
             Our premise is simple: we believe questions and answers are the
             building blocks of conversation, self-expression and deeper
             understanding. As the only global social network built on a Question
@@ -22,13 +22,13 @@ const AboutPage = () => {
             and exploring the social world around you.
           </p>
 
-          <div className='ml-6 text-center'>
+          <div className="ml-6 text-center">
             <Link
-              className='inline-flex items-center py-3 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent ml-11 bg-gradient-to-r from-blue-500 to-blue-800 px-14 text-md md:mt-0 focus:shadow-outline'
+              className="inline-flex items-center py-3 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent ml-11 bg-gradient-to-r from-blue-500 to-blue-800 px-14 text-md md:mt-0 focus:shadow-outline"
               to={'/register'}
             >
-              <div className='flex text-lg'>
-                <span className='justify-center'>Join now</span>
+              <div className="flex text-lg">
+                <span className="justify-center">Join now</span>
               </div>
             </Link>
           </div>
