9b5df1e1cb5ccb5db9a7d029e7bf3972543d1b69
Jay Are Galinada <jayaregalinada@gmail.com>
2022-06-18 19:13:48 +0800

    fix: Incorrect value of mongoose connect

--- server/connect-db.js
+++ server/connect-db.js
@@ -1,13 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export default async (connection, dbName) => {
   try {
-    console.log('Database Connected');
-    await mongoose.connect(
-      connection,
-      { dbName },
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    );
+    console.log("Database Connected");
+    await mongoose.connect(connection, {
+      dbName,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+    });
   } catch (error) {
     console.log(error);
   }
