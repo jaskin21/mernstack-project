19e5f2b657ed673bb12d9766a742d646c9b99908
Jay Are Galinada <jayaregalinada@gmail.com>
2022-06-19 04:29:26 +0800

    fix: Fixed ESLint error

--- client/src/pages/TermsOfService.jsx
+++ client/src/pages/TermsOfService.jsx
@@ -2,36 +2,73 @@ import React from 'react';
 
 const TermsOfService = () => {
   return (
-    <section className='flex justify-center items-center flex-col'>
+    <section className="flex justify-center items-center flex-col">
       <div>
-        <h1 className='font-bold text-2xl pt-10 pb-2'>Terms of Service</h1>
+        <h1 className="font-bold text-2xl pt-10 pb-2">Terms of Service</h1>
       </div>
-      <div className='bg-gray-800 h-auto w-[1000px] p-10 rounded-lg mb-10'>
-        <h1 className='font-bold pb-5 text-lg'>1. Important information</h1>
-        <p className='font-bold'>1a. What are these Terms of Service?</p>
-        <p>These terms ("TOS") are part of the legal papers that regulate your access to and use of SLVL, whether you use the website or the mobile apps (the “Services”).</p>
-        <p className='font-bold pt-5'>1b. Why are these TOU important?</p>
-        <p>You agree to be governed by the terms and conditions of these TOS, as well as the Privacy Policy and Cookie Policy, which are incorporated
-        herein by reference, by accessing, using, registering for, or receiving any of the Services. Because these terms form a legally binding 
-         relationship between you as a user of the Services and SLVL Philippines Limited ("SLVL", "we", "our" or "us"), you must read them before using or using the Services.</p>
-        <p className='font-bold pt-5'>1c. Do I need my parents' consent to sign up or to use SLVL?</p>
-        <p>If you are a minor, you must have your parents' or guardians' consent to enter into these TOU and use the Services, and by using the Services, 
-        you agree that you have received that consent.</p>
-        <h1 className='font-bold pt-10 pb-5 text-lg'>2. Who can use SLVL?</h1>
-        <p className='font-bold'>2a. How old do I need to be?</p>
-        <p>By accessing or using the Services, you represent and warrant that you are at least 13 years old (or the age of legal majority in your country, 
-        if higher), and that you are fully capable of entering into and complying with these TOS.</p>
-        <p className='font-bold pt-5'>2b. What if I am under 13?</p>
-        <p>If you are under the age of 13, you will not be able to use SLVL. You must be at least 13 years old to access or use the Services. If we discover
-        that you are under the age of 13 and are not entitled to use the Services, we will cancel your account and endeavor to prevent you from using the Services.</p>
-        <p className='font-bold pt-5'>2c. What laws do I need to follow?</p>
-        <p>When you access and use the Services, you are responsible for ensuring that you follow all applicable laws and regulations in your home 
-        country. By using the Services, you certify that you have not been convicted of, and are not under any court order relating to, assault, violence, sexual misconduct, or harassment.</p>
-        <p className='font-bold pt-5'>2d. Do you do any background or criminal checks?</p>
-        <p>We do not perform any criminal or other background checks on our users. When engaging with other individuals on the internet, you should use caution, as detailed in our 
-        Safety Center.</p>
+      <div className="bg-gray-800 h-auto w-[1000px] p-10 rounded-lg mb-10">
+        <h1 className="font-bold pb-5 text-lg">1. Important information</h1>
+        <p className="font-bold">1a. What are these Terms of Service?</p>
+        <p>
+          These terms (&quot;TOS") are part of the legal papers that
+          regulate your access to and use of SLVL, whether you use the website
+          or the mobile apps (the “Services”).
+        </p>
+        <p className="font-bold pt-5">1b. Why are these TOU important?</p>
+        <p>
+          You agree to be governed by the terms and conditions of these TOS, as
+          well as the Privacy Policy and Cookie Policy, which are incorporated
+          herein by reference, by accessing, using, registering for, or
+          receiving any of the Services. Because these terms form a legally
+          binding relationship between you as a user of the Services and SLVL
+          Philippines Limited (&quot;SLVL&quot;, &quot;we&quot;, &quot;our&quot;
+          or &quot;us&quot;), you must read them before using or using the
+          Services.
+        </p>
+        <p className="font-bold pt-5">
+          1c. Do I need my parents&apos; consent to sign up or to use SLVL?
+        </p>
+        <p>
+          If you are a minor, you must have your parents&apos; or
+          guardians&apos; consent to enter into these TOU and use the Services,
+          and by using the Services, you agree that you have received that
+          consent.
+        </p>
+        <h1 className="font-bold pt-10 pb-5 text-lg">2. Who can use SLVL?</h1>
+        <p className="font-bold">2a. How old do I need to be?</p>
+        <p>
+          By accessing or using the Services, you represent and warrant that you
+          are at least 13 years old (or the age of legal majority in your
+          country, if higher), and that you are fully capable of entering into
+          and complying with these TOS.
+        </p>
+        <p className="font-bold pt-5">2b. What if I am under 13?</p>
+        <p>
+          If you are under the age of 13, you will not be able to use SLVL. You
+          must be at least 13 years old to access or use the Services. If we
+          discover that you are under the age of 13 and are not entitled to use
+          the Services, we will cancel your account and endeavor to prevent you
+          from using the Services.
+        </p>
+        <p className="font-bold pt-5">2c. What laws do I need to follow?</p>
+        <p>
+          When you access and use the Services, you are responsible for ensuring
+          that you follow all applicable laws and regulations in your home
+          country. By using the Services, you certify that you have not been
+          convicted of, and are not under any court order relating to, assault,
+          violence, sexual misconduct, or harassment.
+        </p>
+        <p className="font-bold pt-5">
+          2d. Do you do any background or criminal checks?
+        </p>
+        <p>
+          We do not perform any criminal or other background checks on our
+          users. When engaging with other individuals on the internet, you
+          should use caution, as detailed in our Safety Center.
+        </p>
       </div>
     </section>
-)};
+  );
+};
 
 export default TermsOfService;
