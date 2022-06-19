2c855b68dc49f1d5520f223770abfef142f0ab8a
Json Almerr <42560435+restbackDev@users.noreply.github.com>
2022-06-19 00:14:06 -0700

    Update ProfileCardComponent.jsx
    added + updated className  and ;

--- client/src/components/ProfileCardComponent.jsx
+++ client/src/components/ProfileCardComponent.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const ProfileCardComponent = () => {
   return (
-    <section class="text-gray-400 body-font">
-      <div class="container py-20 mx-auto">
+    <section className="text-gray-400 body-font">
+      <div className="container py-20 mx-auto">
         {/* border */}
-        <div class="h-full flex items-center border-gray-800 border p-4 rounded-lg"> 
-          <div class="flex flex-wrap -m-4">
-            <div class="p-4 lg:w-5/6">
-              <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200" />
-                <div class="flex-grow sm:pl-8">
-                  <h2 class="title-font font-medium text-lg text-white">Holden Caulfield</h2>
-                  <h3 class="text-gray-500 mb-3">UI Developer</h3>
-                  <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+        <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg"> 
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 lg:w-5/6">
+              <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200" />
+                <div className="flex-grow sm:pl-8">
+                  <h2 className="title-font font-medium text-lg text-white">Holden Caulfield</h2>
+                  <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                  <p className="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                 </div>
               </div>
             </div>
@@ -23,6 +23,6 @@ const ProfileCardComponent = () => {
       </div>
     </section>
   )
-}
+};
 
-export default ProfileCardComponent
+export default ProfileCardComponent;
