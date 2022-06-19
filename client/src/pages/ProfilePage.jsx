756ff33e4e609d1121bff9d75bfc56ef1010ff94
Json Almerr <42560435+restbackDev@users.noreply.github.com>
2022-06-19 00:14:34 -0700

    Update ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React from 'react';
 import ProfileCardComponent from '../components/ProfileCardComponent'
 import QuestionCardComponent from '../components/QuestionCardComponent'
 import AnswerCardComponent from '../components/AnswerCardComponent'
