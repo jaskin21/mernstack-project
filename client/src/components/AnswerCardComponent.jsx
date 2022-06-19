dc2c2ee9a95517f7a20b59321593769d180f0fc2
Jaskin <jaskin.agmata@gmail.com>
2022-06-19 15:37:06 +0800

    Update AnswerCardComponent.jsx

--- client/src/components/AnswerCardComponent.jsx
+++ client/src/components/AnswerCardComponent.jsx
@@ -5,12 +5,11 @@ const AnswerCardComponent = () => {
     <section className="text-gray-400 body-font p-2">
       <div className="container mx-auto">
 
-        {/* border */}
         <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg"> 
           <div className="flex w-full justify-center items-end">
             <div className="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
               <label htmlFor="hero-field" className="leading-7 text-sm text-gray-400">Answer</label>
-              <h3 className='pt-3 font-semibold text-lg text-white'>
+              <h3 className="pt-3 font-semibold text-lg text-white">
                 (Question)Lorem ipsum dolor sit amet
               </h3>
               <p className="pt-2 value-text text-md text-gray-200 fkrr1">
@@ -23,11 +22,10 @@ const AnswerCardComponent = () => {
             </div>
           </div>
         </div>
-        {/* /border */}
 
       </div>
     </section>
-  )
+  );
 };
 
 export default AnswerCardComponent;
