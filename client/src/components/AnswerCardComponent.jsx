fdf90e10722ffc8f978ab880797e8768d0928f40
restbackDev <jason.rbdev@gmail.com>
2022-06-19 20:27:11 -0700

    feat: fixed AnwerCC Alignment

--- client/src/components/AnswerCardComponent.jsx
+++ client/src/components/AnswerCardComponent.jsx
@@ -4,7 +4,7 @@ import ProfileCardQuestion from './cards/ProfileCardQuestion';
 const AnswerCardComponent = () => {
   return (
     <section className="text-gray-400 body-font p-2">
-      <div className="container mx-auto p-4 w-5/6">
+      <div className="container mx-auto py-2">
         <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
           <div className="flex w-full justify-center items-end">
             <div className="relative mr-4 md:w-full text-left">
